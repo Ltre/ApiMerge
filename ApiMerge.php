3d2433426f563c51d467e79f9de6f0cc243c92d5
Ltre Oreki <Ltre@users.noreply.github.com>
2015-06-11 00:43:19 +0800

    删除多余

--- ApiMerge.php
+++ ApiMerge.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * 用于合并API调用的工具
+ * @version 1.0.0
  * @author Ltre<ltrele@yeah.net>
  * @since 2015-06-04
  */
@@ -218,28 +219,7 @@ class ApiMergeTest {
         });
         $am->invoke(array('foo'=>'valueP'));
     }
-    static function test7(){ //客户端原始请求
-        if (! arg('flag')) {
-            $script = '$.get("./?test/apiMerge",{reqList:{req1:[1,2,3],req2:["a","b","c"],req3:null}, flag:1},function(j){},"jsonp")';
-            $html = '<!DOCTYPE html><head><script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script><script>function sendMulti(){'.$script.'}</script></head><body><button onclick="sendMulti()">发送数据</button></body></html>';
-            exit($html);
-        }
-        $am = new ApiMerge();
-        $am->define('req1', function($p1,$p2,$p3){return array('p1'=>++$p1,'p2'=>++$p2,'p3'=>++$p3);});
-        $am->define('req2', function($pa,$pb,$pc){return $pa.$pb.$pc;});
-        $am->define('req3', function(){return !!rand(0, 1);});
-        $util = function($callback) use ($am) {
-            $reqList = arg('reqList');
-            foreach ($reqList as &$r) { $r = (array)$r; }
-            return $am->invoke($reqList, $callback);
-        };
-        @$ret += $util(function($R1,$R2,$R3) use (&$ret){
-            $ret['sha1'] = sha1(serialize($R1).serialize($R2).serialize($R3));
-            $ret['md5'] = md5(serialize($R1).serialize($R2).serialize($R3));
-        });
-        var_dump($ret);
-    }
-    static function test8(){ //封装客户端原始请求
+    static function test7(){ //easyInvoke封装客户端响应
         if (! arg('flag')) {
             $script = '$.get("./?test/apiMerge",{reqList:{req1:[1,2,3],req2:["a","b","c"],req3:null}, flag:1},function(j){},"jsonp")';
             $html = '<!DOCTYPE html><head><script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script><script>function sendMulti(){'.$script.'}</script></head><body><button onclick="sendMulti()">发送数据</button></body></html>';
